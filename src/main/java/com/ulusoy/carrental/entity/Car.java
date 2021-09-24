3478cb1c9edbdf6ea4d5a417af63678ae8ea4e39
osmanulusoyy <osmanulusoyy@hotmail.com>
2021-09-24 17:34:21 +0300

    create packages create Car entity

---
+++ src/main/java/com/ulusoy/carrental/entity/Car.java
@@ -0,0 +1,39 @@
+package com.ulusoy.carrental.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "CAR")
+public class Car {
+    @Id
+    @Column(name = "CAR_ID")
+    private Long carId;
+
+    @Column(name = "CLASS_ID")
+    private Long classId;
+
+    @Column(name = "BRAND")
+    private String brand;
+
+    @Column(name = "SERIES")
+    private String series;
+
+    @Column(name = "MODEL")
+    private String model;
+
+    @Column(name = "DEPOSIT")
+    private BigDecimal deposit;
+
+    @Column(name = "STATUS")
+    private String status;
+}
