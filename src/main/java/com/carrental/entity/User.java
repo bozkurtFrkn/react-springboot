15cf9a92da4098b0108dbff02ca2b18bcabe28ec
osmanulusoyy <osmanulusoyy@hotmail.com>
2021-09-24 20:11:32 +0300

    create - User enetity

---
+++ src/main/java/com/carrental/entity/User.java
@@ -0,0 +1,40 @@
+package com.carrental.entity;
+
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "USER")
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "UserIdSeq")
+    @SequenceGenerator(sequenceName = "USER_ID_SEQ", allocationSize = 1, name = "UserIdSeq")
+    @Column(name = "USER_ID")
+    private Long userId;
+
+    @NotNull
+    @Column(name = "USERNAME")
+    private String username;
+
+    @NotNull
+    @Column(name = "MAIL")
+    private String mail;
+
+    @NotNull
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @NotNull
+    @Column(name = "ROLE")
+    private String role;
+}
